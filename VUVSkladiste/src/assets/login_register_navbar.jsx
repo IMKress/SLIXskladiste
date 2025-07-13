0d3bdb34f84beab7f77cba341e255f2fed54258e
IMKress <162896302+IMKress@users.noreply.github.com>
2025-07-14 00:20:29 +0200

    Add Artikli dropdown

--- VUVSkladiste/src/assets/login_register_navbar.jsx
+++ VUVSkladiste/src/assets/login_register_navbar.jsx
@@ -7,6 +7,9 @@ import logo from './img/logo.png';
 function Navigacija() {
   const [userDetails, setUserDetails] = useState({ username: '', roles: [], UserId: "" });
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [artikliOpen, setArtikliOpen] = useState(false);
+  const [dokumentiOpen, setDokumentiOpen] = useState(false);
+  const [narudzbenicaOpen, setNarudzbenicaOpen] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -52,18 +55,45 @@ function Navigacija() {
        
         <ul className="sidebar-links ">
           <li><Link to="/PocetnaStranica">Početna</Link></li>
-          <li><Link to="/Stanja">Artikli</Link></li>
-          <li><Link to="/Dokumenti">Dokumenti</Link></li>
-          <li><Link to="/Primka">Nova Primka</Link></li>
-          <li><Link to="/Izdatnica">Nova Izdatnica</Link></li>
+          <li>
+            <div className="d-flex justify-content-between" onClick={() => setArtikliOpen(!artikliOpen)}>
+              <Link to="/Stanja">Artikli</Link>
+              <span style={{ cursor: 'pointer' }}>{artikliOpen ? '▲' : '▼'}</span>
+            </div>
+            {artikliOpen && (
+              <ul className="submenu">
+                <li><Link to="/DodajNoviArtikl">Dodaj Artikl</Link></li>
+                <li><Link to="/dodajkategoriju">Dodaj Kategoriju</Link></li>
+              </ul>
+            )}
+          </li>
+          <li>
+            <div className="d-flex justify-content-between" onClick={() => setDokumentiOpen(!dokumentiOpen)}>
+              <Link to="/Dokumenti">Dokumenti</Link>
+              <span style={{ cursor: 'pointer' }}>{dokumentiOpen ? '▲' : '▼'}</span>
+            </div>
+            {dokumentiOpen && (
+              <ul className="submenu">
+                <li><Link to="/Primka">Nova Primka</Link></li>
+                <li><Link to="/Izdatnica">Nova Izdatnica</Link></li>
+              </ul>
+            )}
+          </li>
           <li><Link to="/SkladistePodaci">Podaci o Skladištu</Link></li>
-         
-          
-          <li><Link to="/Narudzbenice">Narudžbenice</Link></li>
-          <li><Link to="/NarudzbenicaNova">Nova Narudžbenica</Link></li>
+          <li>
+            <div className="d-flex justify-content-between" onClick={() => setNarudzbenicaOpen(!narudzbenicaOpen)}>
+              <Link to="/Narudzbenice">Narudžbenice</Link>
+              <span style={{ cursor: 'pointer' }}>{narudzbenicaOpen ? '▲' : '▼'}</span>
+            </div>
+            {narudzbenicaOpen && (
+              <ul className="submenu">
+                <li><Link to="/NarudzbenicaNova">Nova Narudžbenica</Link></li>
+              </ul>
+            )}
+          </li>
           <li><Link to="/Dobavljaci">Dobavljači</Link></li>
           <li><Link to="/Zaposlenici">Zaposlenici</Link></li>
-           <li><Link to="/Statistika">Statistika</Link></li>
+          <li><Link to="/Statistika">Statistika</Link></li>
 
         </ul>
       </div>
